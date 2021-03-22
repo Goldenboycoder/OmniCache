01525dd960587c2a55afc3e68bc9e9217f81bb82
Anthony Tabbal <anthony_tabbal@hotmail.com>
2021-03-22 05:04:24 +0200

    commit on 'gui-branch'

--- driver.py
+++ driver.py
@@ -24,8 +24,9 @@ class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
         exit_ = menu.addAction("Exit")
         exit_.triggered.connect(lambda: sys.exit())  # Adding Exit in tray menu
 
-        # Created an instance of Homepage UI
-        self.Homepage_UI=""
+        # Created an instance of Join Network UI and Homepage UI
+        self.Homepage_UI = None
+        
 
         menu.addSeparator()
         self.setContextMenu(menu)
@@ -50,13 +51,12 @@ OmniCacheApp = QApplication(sys.argv)   # Create Qt Application
 OmniCacheApp.setWindowIcon(QtGui.QIcon('./Images/taskbar_icon.png'))
 
 SplashscreenUI = Ui_splashscreen()      # Create an instance of Splash screen UI
-Join_Network_UI = Ui_JoinNetwork()      # Create an instance of Join Network UI          
-Homepage_UI=""                          # Create an instance of Homepage UI
+Join_Network_UI = Ui_JoinNetwork()      # Create an instance of Join Network UI
+Homepage_UI = None          
 Settings_UI = Ui_settings()             # Create an instance of Settings UI
 
 # Giving the system tray an icon and show()
-tray_icon = SystemTrayIcon(QtGui.QIcon("./Images/currency_logo.png"), Join_Network_UI)
-tray_icon.show()
+tray_icon = SystemTrayIcon(QtGui.QIcon("./Images/currency_logo.png"), Homepage_UI)
 
 # Enabling splash screen function to show
 Ui_splashscreen.flashSplash(SplashscreenUI)
@@ -64,20 +64,32 @@ Ui_splashscreen.flashSplash(SplashscreenUI)
 # Showing Join Network UI after 3s until splash screen is done
 QTimer.singleShot(2000, lambda: Join_Network_UI.show())
 
-# Getting ip and port from Join Network input fields and creating a P2P Node
+# Onclick Listeners
+Join_Network_UI.join_network_btn.clicked.connect(lambda: Join_Network_UI.join_network_btn_onclick(initNode()))   # Triggering Join network button
+Join_Network_UI.ipaddress_input.returnPressed.connect(lambda: Join_Network_UI.join_network_btn_onclick(initNode()))    # Triggering Join network button (Enter pressed)
+
+# Initiliazing node on Join network click
 def initNode():
+
+    global Homepage_UI
+
     ip=Join_Network_UI.ipaddress_input.text()
     port="4444"
     bNode = bcNode("127.0.0.1")
     node= Node(ip,port,bNode)
     Homepage_UI = Ui_homepage(node)
-    Homepage_UI.upload_btn.clicked.connect(lambda: Homepage_UI.upload_onclick())                             # Triggering Upload File button in Homepage
-    Homepage_UI.settings_btn.clicked.connect(lambda: Homepage_UI.settings_onclick(Settings_UI,Homepage_UI))  # Triggering Settings button and passing Settings UI & Homepage UI as param
-    Homepage_UI.search_input.textChanged.connect(lambda: Homepage_UI.on_searchTextChanged(Homepage_UI.search_input.text()))
-    tray_icon.Homepage_UI=Homepage_UI
+    tray_icon.show()
+    HomepageListeners()
     return Homepage_UI
 
-# Onclick Listeners
-Join_Network_UI.join_network_btn.clicked.connect(lambda: Join_Network_UI.join_network_btn_onclick(initNode()))   # Triggering Settings button and passing Settings UI & Homepage UI as param
+
+def HomepageListeners():
+
+    global Homepage_UI
+
+    Homepage_UI.upload_btn.clicked.connect(lambda: Homepage_UI.upload_onclick())                             # Triggering Upload File button in Homepage
+    Homepage_UI.settings_btn.clicked.connect(lambda: Homepage_UI.settings_onclick(Settings_UI,Homepage_UI))  # Triggering Settings button and passing Settings UI & Homepage UI as param
+    Homepage_UI.search_input.textChanged.connect(lambda: Homepage_UI.on_searchTextChanged(Homepage_UI.search_input.text()))  # On text changed in search input
+    tray_icon.Homepage_UI = Homepage_UI
 
 OmniCacheApp.exec_() # Executing app
